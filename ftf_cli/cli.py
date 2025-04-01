7e58c4643cbd851e3bc8338a48dcc33a199678df
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2025-04-01 16:09:04 +0530

    add support to delete module

--- ftf_cli/cli.py
+++ ftf_cli/cli.py
@@ -6,6 +6,7 @@ from ftf_cli.commands.validate_directory import validate_directory
 from ftf_cli.commands.preview_module import preview_module
 from ftf_cli.commands.expose_provider import expose_provider
 from ftf_cli.commands.add_input import add_input
+from ftf_cli.commands.delete_module import delete_module
 
 
 @click.group()
@@ -14,10 +15,11 @@ def cli():
     pass
 
 
-cli.add_command(generate_module)
+cli.add_command(add_input)
 cli.add_command(add_variable)
-cli.add_command(validate_directory)
+cli.add_command(delete_module)
+cli.add_command(expose_provider)
+cli.add_command(generate_module)
 cli.add_command(login)
 cli.add_command(preview_module)
-cli.add_command(expose_provider)
-cli.add_command(add_input)
+cli.add_command(validate_directory)
--- ftf_cli/commands/__init__.py
+++ ftf_cli/commands/__init__.py
@@ -1,7 +1,21 @@
-import click
-
 from .generate_module import generate_module
 from .add_variable import add_variable
-from .validate_directory import validate_directory  # Newly added command import
+from .validate_directory import validate_directory
+from .add_input import add_input
+from .expose_provider import expose_provider
+from .delete_module import delete_module
+from .login import login
+from .preview_module import preview_module
 
-__all__ = ['generate_module', 'add_variable', 'validate_directory']
+# Newly added command import
+
+__all__ = [
+    "add_input",
+    "add_variable",
+    "delete_module",
+    "expose_provider",
+    "generate_module",
+    "login",
+    "preview_module",
+    "validate_directory",
+]
