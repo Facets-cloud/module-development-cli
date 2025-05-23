91ce11bcbbb947cc28341dfdaea6a915437108de
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-23 11:29:10 +0530

    revert t old delete module

--- ftf_cli/commands/delete_module.py
+++ ftf_cli/commands/delete_module.py
@@ -2,30 +2,47 @@ import os
 import traceback
 import click
 import requests
-import yaml
-import json
 
-from ftf_cli.utils import is_logged_in, get_profile_with_priority
+from ftf_cli.utils import is_logged_in
 
 
 @click.command()
-@click.argument("path", type=click.Path(exists=True))
+@click.option(
+    "-i",
+    "--intent",
+    prompt="Intent of the module to delete.",
+    help="Intent of the module to delete.",
+)
+@click.option(
+    "-f",
+    "--flavor",
+    prompt="Flavor of the module to delete.",
+    help="Flavor of the module to delete.",
+)
+@click.option(
+    "-v",
+    "--version",
+    prompt="Version of the module to delete.",
+    help="Version of the module to delete.",
+)
+@click.option(
+    "-s",
+    "--stage",
+    prompt="Stage of the module to delete.",
+    type=click.Choice(["PUBLISHED", "PREVIEW"], case_sensitive=False),
+    help="Stage of the module to delete.",
+)
 @click.option(
     "-p",
     "--profile",
-    default=get_profile_with_priority,
-    help="The profile name to use (defaults to the current default profile)",
+    default=lambda: os.getenv("FACETS_PROFILE", "default"),
+    help="The profile name to use or defaults to environment variable FACETS_PROFILE if set.",
 )
-@click.option(
-    "-c",
-    "--cascade",
-    default=False,
-    help="Delete all versions of the module. Default is false.",
-)
-def delete_module(path, profile, cascade):
-    """Delete a module from the control plane."""
+def delete_module(intent, flavor, version, profile, stage):
+    """Delete a module from the control plane"""
     try:
-        # Check if profile is set
+        stage = stage.upper()
+        # check if profile is set
         click.echo(f"Profile selected: {profile}")
         credentials = is_logged_in(profile)
         if not credentials:
@@ -38,43 +55,8 @@ def delete_module(path, profile, cascade):
         username = credentials["username"]
         token = credentials["token"]
 
-        # Load facets.yaml
-        yaml_file = os.path.join(path, "facets.yaml")
-        if not os.path.exists(yaml_file):
-            raise click.UsageError(f"❌ facets.yaml not found at {path}")
-
-        # Check if facets.yaml exists
-        with open(yaml_file, "r") as file:
-            facets_data = yaml.safe_load(file)
-
-        # Extract intent and flavor
-        intent = facets_data.get("intent")
-        flavor = facets_data.get("flavor")
-        version = facets_data.get("version")
-
-        if not intent or not flavor or not version:
-            raise click.UsageError(
-                "❌ facets.yaml is missing one or more required fields: intent, flavor, version"
-            )
-
-        click.echo(
-            f"Deleting module: intent={intent}, flavor={flavor}, version={version}, cascade={cascade}"
-        )
-
-        # Prompt for confirmation
-        if not click.confirm(
-            f"Are you sure you want to delete {intent}/{flavor}/{version}?"
-        ):
-            click.echo("⚠️ Module deletion cancelled.")
-            return
-
-        # Construct URL with query parameters
-        api_url = f"{control_plane_url}/cc-ui/v1/registry/module/{intent}/{flavor}/{version}"
-        if cascade:
-            api_url += "?cascade=true"
-
         response = requests.get(
-            api_url, auth=(username, token)
+            f"{control_plane_url}/cc-ui/v1/modules", auth=(username, token)
         )
 
         module_id = -1
@@ -89,11 +71,12 @@ def delete_module(path, profile, cascade):
                 filtered_modules.append(module)
 
         for module in filtered_modules:
-            if module["stage"] == "PUBLISHED":
+            if module["stage"] == stage:
                 module_id = module["id"]
                 break
             elif (
-                module["stage"] == "PREVIEW"
+                stage == "PREVIEW"
+                and module["stage"] == "PUBLISHED"
                 and module["previewModuleId"] is not None
             ):
                 module_id = module["previewModuleId"]
